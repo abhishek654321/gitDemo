c13622e0a2f7604e5b31d0d1ff688b1f200f0064
abhishek.a.ashok <abhishek.a.ashok@gmail.com>
2017-03-16 10:19:20 +0530

    sdfsdfsd

--- Demo/src/ProgramOne.java
+++ Demo/src/ProgramOne.java
@@ -1,13 +1,7 @@
 
 public class ProgramOne {
 	public static void main(String[] args) {
-		System.out.println("Let's try git");
-		System.out.println("Let's edit first program");
-
-		System.out.println("Let's edit first program second time from eclipse");
-
-System.out.println("Let's edit first program from bash");
-System.out.println("Let's edit first program from new branch");
+		System.out.println("Hello Abhishek");
 
 	}
 
